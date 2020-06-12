7ccd4e26241a8799ea848bf8431f9bfa0f30bb2d
David Valencia <davidvar@naver.com>
2020-06-12 18:48:51 -0500

    listo

--- Abrir_web_camara/abrir_web_camara.py
+++ Abrir_web_camara/abrir_web_camara.py
@@ -1,15 +1,14 @@
-
 import cv2
 
 cap = cv2.VideoCapture(0)
 
 while(True):
-
+    
     ret, frame = cap.read()
     frame = cv2.resize(frame, None, fx=1.0, fy=1.0)
     cv2.imshow("Doctor Python Tutorial", frame)
     c = cv2.waitKey(1)
-
+    
     # presionar esc para cerrar la ventana
     if c == 27:
     	break 
